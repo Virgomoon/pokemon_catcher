aa6fd2d8d69bd8f19bd59148eee22367e3e5adc9
Joseph Guillory <jagmanx99@yahoo.com>
2023-09-18 00:35:01 -0700

    Searchbar added

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, {useState} from 'react';
+import './App.css';
+
 
 function SearchBar() {
+
+  const [newUser, setNewUser] = useState({
+    username: "",
+    password: "",
+  })
+
+
   return (
     <div className='searchbar'>
-      SearchBar
+      <form className='input'>
+        <label className='label'>Choose a Pokemon</label>
+        <input type="text" className='field'  />
+        <button>SUBMIT</button>
+      </form>
     </div>
   )
 }
