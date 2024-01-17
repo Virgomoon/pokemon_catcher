b3d7207280b7d1cbf49d59e964aa0eaef50c45b0
Joseph Guillory <jagmanx99@yahoo.com>
2024-01-16 21:05:40 -0800

    Add Context and Fetch

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,26 @@
 import './App.css';
 import SearchBar from './SearchBar';
+import React, { useState } from 'react';
+
+export const Context = React.createContext();
 
 function App() {
+
+  // const [newUser, setNewUser] = useState({
+  //   username: "",
+  //   password: "",
+  // })
+
+  const [data, setData] =  useState(null)
+
+  console.log(data)
+
   return (
-    <div className="App">
-      <SearchBar />
-    </div>
+    <Context.Provider value={[data, setData]}>
+      <div className="App">
+        <SearchBar />
+      </div>
+    </Context.Provider>
   );
 }
 
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,20 +1,43 @@
-import React, {useState} from 'react';
+import React, {useContext, useState} from 'react';
 import './App.css';
 
+import { Context } from './App';
 
 function SearchBar() {
 
-  const [newUser, setNewUser] = useState({
-    username: "",
-    password: "",
-  })
+  const url = process.env.REACT_APP_POKE_URL
+  // console.log(url)
 
+  // const [newUser, setNewUser] = useContext(Context)
+  const [pokemon, setPokemon] = useState("")
+  const [data, setData] =  useContext(Context)
+  // console.log(pokemon)
+
+  function handleChange(e) {
+    setPokemon(e.target.value)
+  }
+
+  function handleSubmit(e){
+    e.preventDefault();
+
+      (async function getData(name){
+        const response = await fetch(`${url}${name}`)
+        const dataPromise = await response.json()
+        setData(dataPromise)
+      })(pokemon)
+
+      setPokemon("")
+    }
+    // console.log(data)
 
   return (
     <div className='searchbar'>
-      <form className='input'>
+      <form className='input' onSubmit={handleSubmit}>
         <label className='label'>Choose a Pokemon</label>
-        <input type="text" className='field'  />
+        <input type="text" 
+        className='field' 
+        value={pokemon}
+        onChange={handleChange} />
         <button>SUBMIT</button>
       </form>
     </div>
